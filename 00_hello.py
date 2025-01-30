a08f6ba6d9ed869c8e4e35ae4f777483de1730bd
JuanRisueno <risu.profesional@gmail.com>
2025-01-30 12:31:26 +0100

    Python

---
+++ 00_hello.py
@@ -0,0 +1,19 @@
+#Hola Mundo <--- los comentarios son con #
+#Nuestro Hola Mundo en Python
+print ("Hola Python")
+print ('Hola Python')
+
+#Consultar el tipo de dato
+print(type("Soy un dato str")) #<class 'str'>
+print(type(5)) #<class 'int'>
+print(type(5.5)) #<class 'float'>
+print(type(1 + 2j)) #<class 'complex'>
+print(type(True)) #<class 'bool'>
+
+"""Este 
+es un comentario
+en varias líneas"""
+
+'''Este también
+es un comentario
+en varias líneas'''
---
+++ 01_variables.py
@@ -0,0 +1,5 @@
+# Variables
+
+#Usar _ entre las palabras de la variable
+my_string_variable = "My String Variable"
+print(my_string_variable)
