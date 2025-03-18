745b44935605a9e83d692e3e691b62746da0afbf
JuanRisueno <risu.profesional@gmail.com>
2025-03-18 08:53:33 +0100

    diccionarios

--- 07_dicts.py
+++ 07_dicts.py
@@ -37,3 +37,27 @@ print (my_dict)
 
 print("Pedro" in my_dict) #Realizamos búsquedas y nos devuelve true o false
 print("Apellido" in my_dict)
+
+print(my_dict.items()) #forma de listar los items, keys o valores del diccionario
+print(my_dict.keys())
+print(my_dict.values())
+
+my_list = ["nombre", 1, "Piso"]
+
+my_new_dict = dict.fromkeys((my_list)) #Con fromkeys crea un diccionario sin valores. Aqui a través de una lista.
+print(my_new_dict)
+my_new_dict = dict.fromkeys(("Nombre", 1, "Piso")) #Con fromkeys crea un diccionario sin valores
+print((my_new_dict))
+my_new_dict = dict.fromkeys((my_dict)) #Aqui, con fromkeys, hacemos una copia de las keys de otro diccionario dejando los valores vacíos
+print((my_new_dict))
+my_new_dict = dict.fromkeys(my_dict, "JohnyRisu") #Damos valor Johnyrisu a cada una de las keys del diccionario
+print((my_new_dict))
+
+my_values = my_new_dict.values() #Es una nueva estructura, dict_values
+print(type(my_values))
+
+print(list(my_new_dict)) #Transformarmos el diccionario en otras estructuras
+print(list(my_new_dict.values())) #Transforma en una lista los valores del diccionario
+print(tuple(my_new_dict))
+print(set(my_new_dict))
+
